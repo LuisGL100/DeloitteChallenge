cf9013e1001d7a293917ea38845700e8ef5853a3
Luis Lopez <luisglopez100@gmail.com>
2021-08-08 11:57:57 +1000

    Initial commit

---
+++ FlickrRx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrRx
+//
+//  Created by Luis López on 8/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
